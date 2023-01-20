bda1c30fdefcd011b5b4665f5a9c1a233ada9510
Denis <boss.karelov@gmail.com>
2023-01-20 20:33:52 +0300

    Initial commit

---
+++ coolsite/myself/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+
+from .views import *
+
+urlpatterns = [
+    path('', index),
+    path('resume/<int:resumeid>/', categories),
+    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+]
