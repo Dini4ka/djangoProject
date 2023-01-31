b94048276939e6972192acef96b359b747db265f
Dini4ka <boss.karelov@gmail.com>
2023-01-31 08:14:04 +0300

    Создание нового приложения - Блог

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
         'auth.User',
-        on_delete=models.CASCADE(),
+        on_delete=models.CASCADE,
     )
     body = models.TextField()
 
